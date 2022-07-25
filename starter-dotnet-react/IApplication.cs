887bd5baeabe26e8878d1e9b4399f1ccfc263e70
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-07-24 21:56:58 -0400

    added status enum

--- starter-dotnet-react/IApplication.cs
+++ starter-dotnet-react/IApplication.cs
@@ -1,10 +1,15 @@
+namespace starter_dotnet_react;
 interface IApplication{
     boolean Apply();
     string firstName {get; set;}
     string lastName {get; set;}
     string email {get; set;}
     int creditScore {get; set;}
-    
+
+    enum Status {ACCEPTED, REJECTED, IN_PROGRESS, NOT_SUBMITTED, SUBMITTED}
+
+
+
 
 
 }
